9d45eed647f61b7ab7a45ed5cee724a8d94441fc
Maurizio <proiettomaurizio97@gmail.com>
2021-10-26 16:43:39 +0200

    update

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -4,21 +4,37 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
 // chiedo all'utente di lanciare il dado
-const userDice = parseInt(prompt("Scrivi '1' per lanciare il dado"))
+const userDice = parseInt(prompt("Scrivi '1' per lanciare il dado"));
 
-let dice = document.querySelector("#dices")
+let dice = document.querySelector("#dices");
 console.log(userDice);
+let divCont = document.createElement("div");
+dice.append(divCont);
 
 if (userDice){
     //dado utente
     const userDice = Math.floor(Math.random() * 6 + 1);
-    dice.innerHTML =`${userDice}`
-    console.log("dado user", userDice);
+    divCont.append(`Dado user: ${userDice}`);
+
     //dado cpu
     const cpuDice = Math.floor(Math.random() * 6 + 1);
-    console.log("dado cpu", cpuDice);
-    dice.innerHTML =`${cpuDice}`
+    divCont.append(`Dado cpu: ${cpuDice}`); 
+
+    if (userDice === cpuDice){
+        //avete pareggiato
+        divCont.append("Avete pareggiato!");
+
+    } else if (userDice < cpuDice) {
+        //hai perso
+        divCont.append("Hai perso!");
+
+    } else if (userDice > cpuDice) {
+        //hai vinto
+        divCont.append("Hai vinto!");       
+    }
+   
 }
+
 
     
     
