62a675442a0c7c702a209d33511a5c9cc645315f
Maurizio <proiettomaurizio97@gmail.com>
2021-10-26 15:23:08 +0200

    push for help

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,32 @@
-console.log("ciao");
+/* Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (che hai fatto tu precedentemente), stampa un messaggio appropriato sull’esito del controllo.
+Qui, come detto, NON possiamo usare metodi nuovi sugli array ma lo facciamo con le cose viste questa mattina. */
+
+
+//lista degli utenti
+const guestList = ["maurizio" , "boolean@libero.it" , "a"];
+//contenitore per l'esito 
+const esito = document.querySelector("#esito")
+//chiedo all'utente la sua email
+const userMail = prompt("Scrivimi la tua mail");
+console.log("utente", userMail);
+
+//confronto l'email dell'utente con quelle della lista
+for (let i = 0; i < guestList.length; i++ ){
+    let subscribers = guestList[i];
+    console.log( "log", guestList[i], "variabile", subscribers); 
+
+    if (subscribers === userMail) {
+        //se l'utente è in lista gli dico "sei in lista"
+        console.log("Matching strings!");
+        esito.innerHTML = "sei in lista";
+    } else {
+        // se l'utente non è in lista gli dico: "non sei in lista
+        esito.innerHTML = "non sei in lista";
+    }
+   
+
+
+
+
+
+}
