22797d7a8c1f2c0a18cdf8baaeb4e94ec95c4f76
Maurizio <proiettomaurizio97@gmail.com>
2021-10-26 16:50:17 +0200

    update

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -9,28 +9,36 @@ const userDice = parseInt(prompt("Scrivi '1' per lanciare il dado"));
 let dice = document.querySelector("#dices");
 console.log(userDice);
 let divCont = document.createElement("div");
-dice.append(divCont);
+/* dice.append(divCont); */
 
 if (userDice){
     //dado utente
     const userDice = Math.floor(Math.random() * 6 + 1);
     divCont.append(`Dado user: ${userDice}`);
+    dice.append(divCont);
 
     //dado cpu
     const cpuDice = Math.floor(Math.random() * 6 + 1);
     divCont.append(`Dado cpu: ${cpuDice}`); 
+    dice.append(divCont);
 
     if (userDice === cpuDice){
         //avete pareggiato
+        let divCont = document.createElement("div");
         divCont.append("Avete pareggiato!");
+        dice.append(divCont);
 
     } else if (userDice < cpuDice) {
         //hai perso
+        let divCont = document.createElement("div");
         divCont.append("Hai perso!");
+        dice.append(divCont);
 
     } else if (userDice > cpuDice) {
         //hai vinto
+        let divCont = document.createElement("div");
         divCont.append("Hai vinto!");       
+        dice.append(divCont);  
     }
    
 }
